9646640d5354568b9ae7c40e4f8264a38ae23b44
Gregory Potemkin <greg@pop-os.localdomain>
2021-04-25 23:24:44 +0300

    fixed file search error sql

--- src/storage/db/postgres/files.go
+++ src/storage/db/postgres/files.go
@@ -6,7 +6,7 @@ import (
 
 func (d *manager) GetFileInfoById(id string) (path string, inProgress, isLoaded bool, fLen int64, err error)  {
 	query := `
-SELECT file_name, in_progress, is_loaded, file_length FROM %s WHERE file_id LIKE $1`
+SELECT coalesce(file_name, ''), in_progress, is_loaded, file_length FROM %s WHERE file_id LIKE $1`
 
 	err = d.conn.QueryRow(fmt.Sprintf(query, d.LoadedFilesTablePath()), id).Scan(&path, &inProgress, &isLoaded, &fLen)
 	return path, inProgress, isLoaded, fLen, err
