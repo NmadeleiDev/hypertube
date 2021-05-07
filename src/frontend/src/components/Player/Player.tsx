62699325fa85d087bd9be3dd7f8d17e84bb5c692
bmangust <ftf.pollux@gmail.com>
2021-05-07 13:56:28 +0300

    fixed warnings

--- src/frontend/src/components/Player/Player.tsx
+++ src/frontend/src/components/Player/Player.tsx
@@ -1,5 +1,4 @@
 import { makeStyles } from '@material-ui/core';
-import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import ReactPlayer from 'react-player';
--- src/frontend/src/i18n.ts
+++ src/frontend/src/i18n.ts
@@ -3,7 +3,6 @@ import Backend from 'i18next-http-backend';
 import languageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-const host = window.location.host;
 i18n
   .use(Backend)
   .use(languageDetector)
